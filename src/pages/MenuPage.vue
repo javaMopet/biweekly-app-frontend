6f8886bacd8baea914fc733ab6d4b3788e9e1518
Horacio Peña Mendoza <hpenam@uaemex.mx>
2022-10-17 19:11:39 -0500

    Initial code

---
+++ src/pages/MenuPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="menu__container">
+    <div class="menu__item">
+      <a href="#" class="menu__item--btn">Tienda</a>
+    </div>
+    <div class="menu__item">
+      <a href="#" class="menu__item--btn">Libreria</a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const mivar = ref(1);
+</script>
+<style src="../css/pages/menuPage.scss" />
